57b05aaa42858e93166318a914c8c6e3819aad77
gabogiapp <129702410+gabogiapp@users.noreply.github.com>
2025-07-04 18:56:55 -0400

    bug fix for http link for backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
-const API_URL = 'https://todo-list-frontend-xv35.onrender.com';
+const API_URL = 'https://todo-list-backend-9ok9.onrender.com';
 
 function App() {
   const [todos, setTodos] = useState([]);
