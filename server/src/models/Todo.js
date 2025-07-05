db689585554cd15afe1315e62e0e7f5fc352057f
gabogiapp <129702410+gabogiapp@users.noreply.github.com>
2025-07-04 22:43:39 -0400

    database based off userids

--- server/src/models/Todo.js
+++ server/src/models/Todo.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 const todoSchema = new mongoose.Schema({
   text: { type: String, required: true },
-  completed: { type: Boolean, default: false }
+  completed: { type: Boolean, default: false },
+  userId: { type: String, required: true }
 });
 
 export default mongoose.model('Todo', todoSchema);
--- server/src/routes/todos.js
+++ server/src/routes/todos.js
@@ -5,26 +5,33 @@ const router = express.Router();
 
 // Get all todos
 router.get('/', async (req, res) => {
-  const todos = await Todo.find();
+  const todos = await Todo.find({ userId: req.auth.sub });
   res.json(todos);
 });
 
 // Add a new todo
 router.post('/', async (req, res) => {
-  const newTodo = new Todo(req.body);
+  const newTodo = new Todo({
+    ...req.body,
+    userId: req.auth.sub // Auth0 user ID from JWT
+  });
   const savedTodo = await newTodo.save();
   res.json(savedTodo);
 });
 
 // Update a todo
 router.put('/:id', async (req, res) => {
-  const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  const updatedTodo = await Todo.findOneAndUpdate(
+    { _id: req.params.id, userId: req.auth.sub },
+    req.body,
+    { new: true }
+  );
   res.json(updatedTodo);
 });
 
 // Delete a todo
 router.delete('/:id', async (req, res) => {
-  await Todo.findByIdAndDelete(req.params.id);
+  await Todo.findOneAndDelete({ _id: req.params.id, userId: req.auth.sub });
   res.json({ message: 'Todo deleted' });
 });
 
